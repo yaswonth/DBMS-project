9e57d6923eb3c1134b70b332ea22a43db6df17cc
yaswonth <43050736+yaswonth@users.noreply.github.com>
2020-09-09 22:19:17 +0530

    Update Firebase.js

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 const firebaseConfig = {
-    apiKey: "AIzaSyAUe7Ds6VyHAuzXCC03YnytHqeUAJtzbO8",
+    apiKey: "",
   authDomain: "shyam-4ff74.firebaseapp.com",
   databaseURL: "https://shyam-4ff74.firebaseio.com",
   projectId: "shyam-4ff74",
-  storageBucket: "shyam-4ff74.appspot.com",
-  messagingSenderId: "118713083232",
-  appId: "1:118713083232:web:912eda49bba13430402a05",
-  measurementId: "G-19QGH462QF"
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
   };
 
   firebase.initializeApp(firebaseConfig);
